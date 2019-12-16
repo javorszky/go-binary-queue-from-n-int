d4d24c92193e0a0de5fccfea3b1c0c74ad1088a9
Gabor Javorszky <gabor@javorszky.co.uk>
2019-12-16 14:07:51 +0000

    Export funcs

--- binaryq.go
+++ binaryq.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 )
 
-func binaryQueueConvert(num int64) *list.List {
+func BinaryQueueConvert(num int64) *list.List {
 	l := list.New()
 	for i := int64(0); i <= num; i++  {
 		l.PushBack(strconv.FormatInt(i, 2))
@@ -15,7 +15,7 @@ func binaryQueueConvert(num int64) *list.List {
 	return l
 }
 
-func binaryQueueDisplay(num int) *list.List {
+func BinaryQueueDisplay(num int) *list.List {
 	l := list.New()
 	for i := int(0); i <= num; i++  {
 		l.PushBack(i)
@@ -26,14 +26,14 @@ func binaryQueueDisplay(num int) *list.List {
 
 
 func main() {
-	queue := binaryQueueConvert(20)
+	queue := BinaryQueueConvert(20)
 	for e := queue.Front(); e != nil; e = e.Next() {
 		fmt.Println(e.Value);
 	}
 
 	fmt.Println("Displaying the next version")
 
-	queue2 := binaryQueueDisplay(20)
+	queue2 := BinaryQueueDisplay(20)
 	for f := queue2.Front(); f != nil; f = f.Next() {
 		fmt.Println(fmt.Sprintf("%b", f.Value))
 	}
